5e94993ad346861ec4270c36fddafe878b58bca0
mariocyber17 <marioudea1988@gmail.com>
2025-11-14 23:34:00 -0500

    agregué la base de la tarea

---
+++ app.js
@@ -0,0 +1,17 @@
+/*let entrada = prompt("Ingresa la temperatura en celsius:");
+console.log(entrada)
+let temperatura = Number(entrada);// para hacer el número de entrada un numero real y que no lo vea como cadena
+console.log(temperatura)
+while (isNaN(temperatura)) { //isNaN nos dice si una variable no es número
+    console.log("lo que ingresaste no es un número")
+    entrada = prompt("Ingresa la temperatura en celsius:");
+    console.log(entrada)
+    temperatura = Number(entrada);
+}
+
+let fahrenheit = (temperatura * (9/5))+32;
+console.log(fahrenheit)
+let kelvin = temperatura + 273.15;
+console.log(kelvin)
+*/
+
